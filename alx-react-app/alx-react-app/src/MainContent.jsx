c0a38c0960962b88a5c645242d8665890abc4f54
Oualid Elyaagoubi <oualidelyaagoubi@ouatlook.com>
2024-11-04 01:08:14 +0100

    all done

---
+++ alx-react-app/alx-react-app/src/MainContent.jsx
@@ -0,0 +1,14 @@
+
+function MainContent() {
+    return (
+        <div>
+        reate a functional component that returns the following JSX element:
+
+<main>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+        </div>
+    );
+}
+
+export default MainContent;
