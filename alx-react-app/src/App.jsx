7e113ca3d005d3028aba9635368089cb68e48f5e
Oualid Elyaagoubi <oualidelyaagoubi@ouatlook.com>
2024-11-04 02:09:55 +0100

    all dokok

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Header'
-import Footer from './Footer'
-import MainContent from './MainContent'
-import WelcomeMessage from './WelcomeMessage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import MainContent from './components/MainContent'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
