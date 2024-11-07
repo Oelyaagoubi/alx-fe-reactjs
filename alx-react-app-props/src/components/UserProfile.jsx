62a4e0d8fe89c37809c81a25d2090bcbf0492fcf
Oualid Elyaagoubi <oualidelyaagoubi@ouatlook.com>
2024-11-07 17:58:14 +0100

    task number 3 doned f

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,8 +1,16 @@
-import UserInfo from './UserInfo';
-import UserContext from './UserContext';
 
-function ProfilePage() {
-  return <UserInfo  />;
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserDetails() {
+
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserDetails;
