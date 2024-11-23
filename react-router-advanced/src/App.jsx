9ac9355bf8d5cb099906b3b7296261c23aa255be
Oualid Elyaagoubi <oualidelyaagoubi@ouatlook.com>
2024-11-23 03:47:09 +0100

    react root finish

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
 
-function ProtectedRoute({ children, isAuthenticated }) {
-  return isAuthenticated ? children : <Navigate to="/login" />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post/:id" element={<Post />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default ProtectedRoute;
+export default App;
